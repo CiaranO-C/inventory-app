3ddc36902df16ddf9e0c1e5c08d43ea6a792e2c0
CiaranOC <129303811+CiaranO-C@users.noreply.github.com>
2024-07-26 09:52:22 +0100

    ""

--- routes/categoriesRouter.js
+++ routes/categoriesRouter.js
@@ -3,6 +3,14 @@ const categoryController = require("../controllers/categoryController");
 
 const categoriesRouter = Router();
 
-categoriesRouter.get('/', categoryController.categoriesGet)
+categoriesRouter.get("/", categoryController.allCategoriesGet);
+
+categoriesRouter.get("/create", categoryController.createCategoryGet);
+categoriesRouter.post("/create", categoryController.createCategoryPost);
+
+categoriesRouter.get("/:id", categoryController.singleCategoryGet);
+
+categoriesRouter.get('/:id/update', categoryController.updateCategoryGet);
+categoriesRouter.post('/:id/update', categoryController.updateCategoryPost);
 
 module.exports = categoriesRouter;
