9b0e2a713b11124950474e56110ab1035dec1607
CiaranOC <129303811+CiaranO-C@users.noreply.github.com>
2024-07-26 09:54:32 +0100

    write dash get controller, displays all categories

--- controllers/dashController.js
+++ controllers/dashController.js
@@ -1,5 +1,12 @@
-function dashboardGet(req, res, next) {
-    res.render('dashboard')
-}
+const asyncHandler = require("express-async-handler");
+const db = require("../db/pool");
+
+const dashboardGet = asyncHandler(async (req, res, next) => {
+  const categories = await db.query("SELECT * FROM categories");
+  res.render("dashboard", {
+    title: "Dashboard",
+    categories: categories.rows,
+  });
+});
 
 module.exports = { dashboardGet };
