c71650bb06d1a834657ec2462a0ceb3095dfeea2
CiaranOC <129303811+CiaranO-C@users.noreply.github.com>
2024-07-26 09:53:31 +0100

    remove async handler from query function, change to arrow func

--- db/pool.js
+++ db/pool.js
@@ -5,10 +5,10 @@ const pool = new Pool({
   connectionString: process.env.CONNECTION_STRING,
 });
 
-async function query(sql, params) {
-  console.log("im here!");
+const query = async (sql , params) => {
+  console.log("DB Query Sent");
   const res = await pool.query(sql, params);
   return res;
-}
+};
 
 module.exports = { query };
