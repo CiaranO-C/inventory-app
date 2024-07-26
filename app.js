6e03412dc16bd44366eaa59a3a1e57a7eff71d6c
CiaranOC <129303811+CiaranO-C@users.noreply.github.com>
2024-07-26 16:58:31 +0100

    add use statement for admin router

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 const express = require("express");
-const indexRouter = require('./routes/indexRouter')
-const dashboardRouter = require('./routes/dashboardRouter');
+const indexRouter = require("./routes/indexRouter");
+const dashboardRouter = require("./routes/dashboardRouter");
+const adminRouter = require('./routes/adminRouter')
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 5050;
 
 app.set("view engine", "ejs");
 
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/', indexRouter)
-app.use('/dashboard', dashboardRouter)
+app.use("/", indexRouter);
+app.use("/dashboard", dashboardRouter);
+app.use('/admin', adminRouter)
 
 app.use((err, req, res, next) => {
+
   if (err.statusCode) {
     console.error(err.statusCode, err.message);
     return;
